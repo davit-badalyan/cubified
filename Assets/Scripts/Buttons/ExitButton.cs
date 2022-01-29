b79a2956ebee736bf806444cd12bed5dbf744910
Davit Badalyan <davit.badalyan.00@gmail.com>
2022-01-29 04:24:34 -0800

    Levels updated, Start Menu and Credits scenes added

---
+++ Assets/Scripts/Buttons/ExitButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Start()
+    {
+        //
+    }
+
+    private void Update()
+    {
+        //
+    }
+}
---
+++ Assets/Scripts/Buttons/StartButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Start()
+    {
+        //
+    }
+
+    private void Update()
+    {
+        //
+    }
+}
